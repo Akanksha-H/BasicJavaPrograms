493bc93ab366cd3000618cbab6e9f37cc31fe217
Aakanksha <aakanksha0499@gmail.com>
2022-06-14 08:43:10 +0530

    Prime Factors Program

--- src/basiccorejavaprograms/BasicCorePrograms.java
+++ src/basiccorejavaprograms/BasicCorePrograms.java
@@ -5,21 +5,23 @@ import java.lang.Math;
 
 public class BasicCorePrograms {
     public static void main(String[] args) {
-        harmonicNumber();
+        primFactors();
     }
 
-    static double harmonicNumber() {
-        float harmonic = 1;
-        System.out.println("Enter the value of number");
+    static void primFactors() {
+        int number;
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        System.out.println("Enter a number :");
+        number = sc.nextInt();
 
-        // loop to apply the formula
-        // Hn = H1 + H2 + H3 ... + Hn-1 + Hn-1 + 1/n
-        for (int i = 2; i <= number; i++) {
-            harmonic += (float) 1 / i;
-            System.out.println("Value of harmonic number: " + harmonic);
+        for (int i = 2; i < number; i++) {
+            while (number % i == 0) {
+                System.out.println(i + " ");
+                number = number / i;
+            }
         }
-        return harmonic;
+        if (number > 2) {
+            System.out.println(number);
+        }
     }
 }
